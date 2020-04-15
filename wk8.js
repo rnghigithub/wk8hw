fd9f9feeae50a0ef2c0b4dfbf899772ceeec9080
Rebecca Nghi <60793465+rnghigithub@users.noreply.github.com>
2020-04-15 00:04:44 -0400

    Add files via upload

--- wk8.js
+++ wk8.js
@@ -1,1 +1,17 @@
+function getData() {
+    var movie = document.getElementById(`movie`).value;
+    var container = document.getElementById(`container`);
+    var url = `https://www.omdbapi.com/?apikey=YOURAPIKEY&t=${movie}`;
+    var xhttps = new XMLHttpRequest();       
+}
 
+
+xhttps.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        console.log(this.responseText);
+        console.log(JSON.parse(this.responseText));
+
+        const data = JSON.parse(this.responseText);
+
+    }
+};
